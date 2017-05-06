674f6a31749c9a32e40bf45dedcc9eeb8561d3ca
sjones88 <steven.g.jones88@gmail.com>
2017-05-06 14:45:12 -0500

    get rid of driver code in binary_tree_node.rb

--- Trees/binary_tree_node.rb
+++ Trees/binary_tree_node.rb
@@ -14,15 +14,3 @@ class Node
 		end
 	end
 end
-
-five = Node.new(5)
-three = Node.new(3)
-seven = Node.new(7)
-five.left = three
-five.right = seven
-
-search = Proc.new do |n|
-		puts n.data * 3
-	end
-
-Node.traverse(five, &search)
